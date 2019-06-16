2b00626deff30960dc2d76df8e3f11d9227dad7a
nayebare <mnayebare@rwanda.cmu.edu>
2019-06-16 13:44:36 +0300

    enforcing cohension in unit tests

---
+++ app/models/user.test.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const app = require("../../server");
+const User = mongoose.model("User");
+const Article = mongoose.model("Article");
+const  bcrypt = require('bcryptjs');
+
+
+describe("Method Save User", function() {
+  beforeAll(() => {
+  if(mongoose.createConnection(process.env.MONGOHQ_URL)){
+  }});
+
+    it("expect password string to match encrupted string", async () => {
+      await new User({
+        name: "name",
+        email: "test@test.com",
+        password: "password",
+        username: "username"
+      }).authenticate("password")
+      {
+        expect(true).toBe(true)     
+      }
+  });
+
+  it("username should  not be empty", async () => {
+    await new User({
+      name: "name",
+      email: "test@test.com",
+      password: "password",
+      username: ""
+    }).save(function(error){
+        expect(err.message).toEqual("Validation failed"); 
+    })
+});
+
+  afterAll((done) => {
+    mongoose.disconnect(done);
+  });
+});
+
+ 
